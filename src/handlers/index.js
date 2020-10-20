942084ca3cda75719197b7f820437c3361a70de4
ZHENK <chengzhenyang@gmail.com>
2020-10-20 21:34:07 +1300

    add socket.io

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -1,10 +1,10 @@
 import { getLeaderboard, postLeaderboard, getHighScore } from './leaderboard'
 import { postLogin, postRegister, getSelf } from './user'
-import { getHome } from './home'
+import { getHome, unimplemented } from './home'
 
 export {
     getLeaderboard, postLeaderboard,
-    getHome,
+    getHome, unimplemented,
     postLogin, postRegister, getSelf,
-    getHighScore
+    getHighScore,
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
+import socket from 'socket.io'
+import http from 'http'
 
 import {
     getLeaderboard, postLeaderboard, getHighScore,
-    getHome,
+    getHome, unimplemented,
     postRegister, postLogin, getSelf
 } from './handlers'
 
@@ -30,12 +32,34 @@ app.get('/leaderboard', getLeaderboard)
 app.get('/highscore', getHighScore)
 app.post('/leaderboard', auth, postLeaderboard)
 
+app.post('/host', unimplemented)
+app.get('/lobby', unimplemented)
+
 app.get('/self', auth, getSelf)
 app.post('/login', postLogin)
 app.post('/register', postRegister)
 
+const server = http.createServer(app)
+const io = socket(server)
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    console.log(socket.id)
+    client.set("mastersocket", socket.id, function(err) {
+        if (err) throw err;
+        console.log("Master socket is now" + socket.id);
+      });
+
+    socket.on('chat message', function (msg) {
+        console.log('message: ' + msg);
+    });
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+
+server.listen(port, () => {
     console.log(`Quinzical app listening at http://localhost:${port}`)
 })
 
